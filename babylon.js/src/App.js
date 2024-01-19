f5d7d5f1dc3939a67c24c13ccae185d84a0a3250
heejinssss <heejin.dev33@gmail.com>
2024-01-20 03:51:38 +0900

    폴더명 변경

---
+++ babylon.js/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import BabylonBoard from "./pages/babylon";
+
+function App() {
+  const pin = 3;
+
+  return (
+    <div className="App">
+      <BabylonBoard pin={pin} />
+    </div>
+  );
+}
+
+export default App;
